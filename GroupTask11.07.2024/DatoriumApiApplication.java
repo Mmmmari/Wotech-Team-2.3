7d1c5f52d2a2bca515509735ebdc4e9a127e0b6d
Mari <165931583+Mmmmari@users.noreply.github.com>
2024-07-11 21:01:08 +0300

    Update DatoriumApiApplication.java

--- GroupTask11.07.2024/DatoriumApiApplication.java
+++ GroupTask11.07.2024/DatoriumApiApplication.java
@@ -1,5 +1,3 @@
-//MAIN
-
 package com.datorium.Datorium.API;
 
 import org.springframework.boot.SpringApplication;
@@ -65,26 +63,4 @@ public class DatoriumApiApplication {
 }
 
 
-
-//SUMMER
-
-package com.datorium.Datorium.API;
-
-public class Summer{
-    public float temperature;
-    public String activity;
-    public boolean isHotEnoughForBeach;
-
-    public Summer(float temperature) {
-        this.temperature = temperature;
-    }
-
-    public void setActivity(String activity) {
-        this.activity = activity;
-    }
-
-    public void setBeachWeather(boolean isBeachWeather) {
-        this.isHotEnoughForBeach = isBeachWeather;
-    }
-}
 
