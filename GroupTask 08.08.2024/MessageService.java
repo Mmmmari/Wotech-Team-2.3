670fb62e3ea14c8315561cc88e50ad6fc5c75392
Mari <165931583+Mmmmari@users.noreply.github.com>
2024-08-08 20:49:12 +0300

    Create MessageService.java

---
+++ GroupTask 08.08.2024/MessageService.java
@@ -0,0 +1,11 @@
+package com.datorium.Datorium.API.Services;
+
+import com.datorium.Datorium.API.DTOs.Message;
+
+public class MessageService {
+
+    public int reply(Message message){
+        return 444;
+    }
+
+}
