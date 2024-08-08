538982b96a4c2af1289c4584db7a7103a3c14c61
Mari <165931583+Mmmmari@users.noreply.github.com>
2024-08-08 20:49:41 +0300

    Create Message.java

---
+++ GroupTask 08.08.2024/Message.java
@@ -0,0 +1,11 @@
+package com.datorium.Datorium.API.DTOs;
+
+import com.datorium.Datorium.API.DTOs.Message;
+
+public class Message {
+
+    public int shout(Message message){
+        return 555;
+    }
+
+}
